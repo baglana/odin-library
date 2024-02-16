d2daf5bfd4bc9983e555bff107af9a1aba6105fb
baglana <102351104+baglana@users.noreply.github.com>
2024-02-16 18:18:05 +0600

    Change Book properties to private

--- book.js
+++ book.js
@@ -1,20 +1,27 @@
 class Book {
-  constructor(title, author, pages, isRead = false) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+  #title;
+  #author;
+  #pages;
+  #isRead = false;
+
+  constructor(title, author, pages, isRead) {
+    console.log(title, this.#title);
+    this.#title = title;
+    console.log(title, this.#title);
+    this.#author = author;
+    this.#pages = pages;
+    this.#isRead = isRead;
   }
 
   getInfo() {
-    let info = `${this.title} by ${this.author}, ${this.pages} pages`
-    if (!this.isRead) {
+    let info = `${this.#title} by ${this.#author}, ${this.#pages} pages`
+    if (!this.#isRead) {
         info += ', not read yet'
     }
     return info;
   }
 
   toggleReadStatus() {
-    this.isRead = !this.isRead;
+    this.#isRead = !this.#isRead;
   }
 }
